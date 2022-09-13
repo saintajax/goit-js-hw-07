494e5dd312d5637e06c965915122a782eab0d43f
saintajax <saintajax@gmail.com>
2022-09-13 21:19:05 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const galleryItemMarkup = makeGalleryItemMarkup(galleryItems).join("");
+
+function makeGalleryItemMarkup(galleryItems = {}) {
+  return galleryItems.map(({ description, original, preview }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+    loading="lazy"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  });
+}
+
+refs.gallery.innerHTML = galleryItemMarkup;
+
+refs.gallery.addEventListener("click", onOpenModal);
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+}
