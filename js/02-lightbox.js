e7fd71c6db1d5c0641ff247676aa8e7f5092cf55
saintajax <saintajax@gmail.com>
2022-09-13 22:57:50 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const galleryItemMarkup = makeGalleryItemMarkup(galleryItems).join("");
+
+function makeGalleryItemMarkup(galleryItems = {}) {
+  return galleryItems.map(({ description, original, preview }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" loading="lazy" src="${preview}" alt="${description}" />
+</a>`;
+  });
+}
+
+refs.gallery.innerHTML = galleryItemMarkup;
+
+// refs.gallery.addEventListener("click", onOpenModal);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
